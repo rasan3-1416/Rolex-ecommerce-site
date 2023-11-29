54faa0bd97056ae0d24931aac5056fbb66bc0759
rasan3-1416 <md.rakibhasan3.1416@gmail.com>
2023-11-29 10:48:07 +0600

    feat(active-section-indication): Add to the site

--- src/js/main.js
+++ src/js/main.js
@@ -84,22 +84,24 @@ let newSwiper = new Swiper('.new-swiper', {
 })
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
-// const sections = document.querySelectorAll('section[id]')
-// function scrollActive(){
-//     const scrollY = window.pageYOffset
-//     sections.forEach((current) => {
-//         const sectionHeight = current.offsetHeight,
-//               sectionTop = current.offsetTop - 58,
-//               sectionId = current.getAttribute('id'),
-//               scrolledTo = document.querySelector(`.nav__menu a[href*= ${sectionId}]`)
-//         if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-//             scrolledTo.classList.add('active-link')
-//         }else{
-//             scrolledTo.classList.remove('active-link')
-//         }
-//     })
-// }
-// window.addEventListener('scroll', scrollActive)
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+    sections.forEach((current) => {
+        const sectionHeight = current.offsetHeight,
+              sectionTop = current.offsetTop - 58,
+              sectionId = current.getAttribute('id'),
+              scrolledTo = document.querySelector(`#nav-menu a[href*= ${sectionId}]`)
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            scrolledTo.classList.add('dynamic-style')
+        }else{
+            scrolledTo.classList.remove('dynamic-style')
+        }
+    })
+}
+
+window.addEventListener('scroll', scrollActive)
 
 /*=============== SHOW SCROLL UP ===============*/ 
 function scrollUp(){
